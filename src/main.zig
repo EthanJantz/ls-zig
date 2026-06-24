bc614df80c259a08d81aebe04a11f9367bafcbff
EthanJantz <21095848+EthanJantz@users.noreply.github.com>
2026-06-24 15:50:09 -0500

    make time and mode human readable

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,46 @@
 const std = @import("std");
 const linux = std.os.linux;
+const POSIX_S = std.posix.S;
+const zeit = @import("zeit");
+
+pub fn make_mode_human_readable(mode: u16) [10:0] u8 {
+    var str: [10:0] u8 = std.mem.zeroes([10:0]u8);
+    
+    switch(mode & POSIX_S.IFMT) {
+        POSIX_S.IFDIR => str[0] = 'd',
+        POSIX_S.IFLNK => str[0] = 'l',
+        POSIX_S.IFCHR => str[0] = 'c',
+        POSIX_S.IFBLK => str[0] = 'b',
+        POSIX_S.IFIFO => str[0] = 'p',
+        POSIX_S.IFSOCK => str[0] = 's',
+        else => str[0] = '-'
+    }
+    
+    str[1] = if(mode & POSIX_S.IRUSR != 0) 'r' else '-';
+    str[2] = if(mode & POSIX_S.IWUSR != 0) 'w' else '-';
+    str[3] = if(mode & POSIX_S.IXUSR != 0) 'x' else '-';
+
+    str[4] = if(mode & POSIX_S.IRGRP != 0) 'r' else '-';
+    str[5] = if(mode & POSIX_S.IWGRP != 0) 'w' else '-';
+    str[6] = if(mode & POSIX_S.IXGRP != 0) 'x' else '-';
+
+    str[7] = if(mode & POSIX_S.IROTH != 0) 'r' else '-';
+    str[8] = if(mode & POSIX_S.IWOTH != 0) 'w' else '-';
+    str[9] = if(mode & POSIX_S.IXOTH != 0) 'x' else '-';
+
+    return str;
+}
+
+pub fn format_unix_time(epoch_seconds: i64) ![16]u8 {
+    const es: std.time.epoch.EpochSeconds = .{.secs = @intCast(epoch_seconds)};
+    const day = es.getEpochDay().calculateYearDay();
+    const md = day.calculateMonthDay();
+    const ds = es.getDaySeconds();
+
+    var buf: [16]u8 = undefined;
+    _ = std.fmt.bufPrint(&buf, "{d}-{d:0>2}-{d:0>2} {d:0>2}:{d:0>2}", .{day.year, md.month.numeric(), md.day_index + 1, ds.getHoursIntoDay(), ds.getMinutesIntoHour()}) catch unreachable;
+    return buf;
+}
 
 pub fn main(init: std.process.Init) !void {
     const args = try init.minimal.args.toSlice(init.arena.allocator());
@@ -57,7 +98,7 @@ pub fn main(init: std.process.Init) !void {
                         .SUCCESS => {},
                         else => return error.StatFailed,
                     }
-                    try stdout_writer.print("{x} {x} {x} {x} {s}\n", .{statx.mode, statx.gid, statx.uid, statx.mtime.sec, entry.name});
+                    try stdout_writer.print("{s} {x} {x} {s} {s}\n", .{make_mode_human_readable(statx.mode), statx.gid, statx.uid, try format_unix_time(statx.mtime.sec), entry.name});
                     try stdout_writer.flush();
                 },
                 .file => {
@@ -76,7 +117,9 @@ pub fn main(init: std.process.Init) !void {
                         .SUCCESS => {},
                         else => return error.StatFailed,
                     }
-                    try stdout_writer.print("{x} {x} {x} {x} {s}\n", .{statx.mode, statx.gid, statx.uid, statx.mtime.sec, entry.name});
+                    
+
+                    try stdout_writer.print("{s} {x} {x} {s} {s}\n", .{make_mode_human_readable(statx.mode), statx.gid, statx.uid, try format_unix_time(statx.mtime.sec), entry.name});
                     try stdout_writer.flush();
                 },
                 else => {}
